a915f05641ea92d766301f6e61ee9c8848dc8b42
prplecake <matthew@veganmsp.com>
2023-11-27 17:53:02 -0600

    Handle undefined string

--- src/functions/HtmlUtils.ts
+++ src/functions/HtmlUtils.ts
@@ -1,9 +1,9 @@
 import DOMPurify from "dompurify";
 
-export const escapedNewLineToLineBreakTag = (string: string): string[] => {
-  return string.split("\n").map((item, index) => {
+export const escapedNewLineToLineBreakTag = (string?: string): string[] => {
+  return string?.split("\n").map((item, index) => {
     return (index === 0) ? item : `<br/>${item}`;
-  });
+  }) ?? [];
 };
 export const sanitizeHTML = (content: string) => () => ({
   __html: DOMPurify.sanitize(
