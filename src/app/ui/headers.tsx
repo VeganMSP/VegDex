5112f73f603e1d03726e9b206b456cb5ee07d615
Matty Jorgensen <matthew@veganmsp.com>
2023-11-27 22:07:19 -0600

    Remove color from mobile menu logo link

--- src/app/ui/headers.tsx
+++ src/app/ui/headers.tsx
@@ -48,7 +48,7 @@ export const Header = () => {
         <div className="fixed inset-0 z-10" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <a href="/" className="-m-1.5 p-1.5 text-lg font-bold">
+            <a href="/" className="-m-1.5 p-1.5 text-lg text-gray-900 font-bold">
               VegDex
             </a>
             <button
